fa226e5a871a279c7c96358d41a1e02c7f4dd45d
Krisanapong Jareunjanporn <150851052+Krianapong@users.noreply.github.com>
2023-12-09 22:47:22 +0700

    Updat

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 // import './App.css'
 import Books from './Books'
+import Page from './Page'
 
 function App() {
   return (
     <>
-      <Books />
+      {/* <Books /> */}
+      <Page />
     </>
   )
 }
